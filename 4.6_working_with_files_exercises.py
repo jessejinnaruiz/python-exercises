3780723f34adf7778dbf6c31d3c1233e14998ef7
Jesse Ruiz <jessejinnaruiz@gmail.com>
2019-03-08 08:48:28 -0600

    First commit

---
+++ 4.6_working_with_files_exercises.py
@@ -0,0 +1,56 @@
+# Read the contents of your last exercise file into a variable.
+
+# with open('4.5_import_exercises.py') as f:
+#     file_contents = f.read()
+# print(file_contents)
+
+# Print out every line in the file
+
+# Print out every line in the file, but add line numbers
+# with open('4.5_import_exercises.py') as f:
+#     file_contents = f.readlines()
+
+# n=1
+# for line in range(0,len(file_contents)-1):
+#     print('line number is ', n, file_contents[n])
+#     n += 1
+
+# Write some python code to create a grocery list. -------------------------------------
+
+# Create a variable named grocery_list. 
+# It should be a list, and the elements in the list should be a least 3 things that you need to buy from the grocery store.
+grocery_list = ['zucchini', 'celery', 'avocados']
+
+# Create a function named make_grocery_list. 
+# When run, this function should write the contents of the grocery_list variable to a file named my_grocery_list.txt.
+
+def make_grocery_list():
+    with open('my_grocery_list.txt', 'w') as f:
+        for elements in grocery_list:
+                f.write("%s \n" %elements)
+make_grocery_list()
+
+# Create a function named show_grocery_list. When run, it should show each item on the grocery list.
+def show_grocery_list():
+        with open('my_grocery_list.txt') as f:
+                lines = f.read()
+                print(lines)
+                # for line in f.readlines():     
+                #         print(line)
+
+show_grocery_list()
+
+# Create a function named buy_item. It should accept the name of an item on the grocery 
+# list, and remove that item from the list.
+
+def buy_item(item):
+        with open('my_grocery_list.txt') as f:
+                lines = f.read()
+                for line in lines:
+                        if item in lines:
+                                line.replace(item, '')
+                print(lines)
+                # for line in f.readlines():     
+                #         print(line)
+print('testing buy_item function ---------')
+buy_item('celery')
